e174133f3ccdc668bfb38804c14a7eb456462fa4
Pierre De Wulf <pierredewulf31@gmail.com>
2015-03-28 17:10:18 +0100

    #5 Execution requete Select ok

--- src/main/java/JlibPdewWdum/Tools/bddRequest.java
+++ src/main/java/JlibPdewWdum/Tools/bddRequest.java
@@ -1,8 +1,10 @@
 package JlibPdewWdum.Tools;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * Created by daolf on 28/03/15.
@@ -10,27 +12,37 @@ import java.sql.ResultSet;
 public class bddRequest {
 
     private static final String sJdbc = "jdbc:sqlite";
-    //TODO change regarding the user
+    //TODO a changer en fonction de votre machine
     private static final String dbLocation = "/Users/daolf/Dropbox/Cour/S8/INTEROP/IAWS-JlibPdewWdum-ProjetWS/database/bdd.db";
 
-    public static final ResultSet sendRequest() {
+
+    public static final ResultSet sendRequest(String req) {
 
         Connection c = null;
+        Statement stmt = null;
+        ResultSet rs = null;
         try {
             Class.forName("org.sqlite.JDBC");
             String dbUrl = sJdbc + ":" + dbLocation;
             c = DriverManager.getConnection(dbUrl);
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            stmt = c.createStatement();
+            rs = stmt.executeQuery(req);
+            rs.close();
+            stmt.close();
+            c.close();
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
-        return null;
+        System.out.println("Request send successfully : " + req);
+
+        return rs;
     }
 
 
     public static void main( String args[] )
     {
-        sendRequest();
+        sendRequest("SELECT * FROM TECHNO");
     }
 }
