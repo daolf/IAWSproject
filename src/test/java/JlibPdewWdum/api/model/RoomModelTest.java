4863c1d352ea8f5cfabf61530d6e8b729eeef78b
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-01 15:14:40 +0200

    ADD RoomModelTest

--- src/test/java/JlibPdewWdum/api/model/RoomModelTest.java
+++ src/test/java/JlibPdewWdum/api/model/RoomModelTest.java
@@ -1,7 +1,28 @@
 package JlibPdewWdum.api.model;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 
 public class RoomModelTest extends TestCase {
+
+
+    public RoomModel r;
+    @Before
+    public void setup() {
+        r = new RoomModel(1,2,10);
+    }
+
+    public void testGetter() {
+        assertEquals("getIdRoom",1,r.getIdRoom());
+        assertEquals("getIdCinema",2,r.getIdCinema());
+        assertEquals("getNbPlaceRoom",10,r.getIdRomm());
+    }
+
+    public void testSetter(){
+        r.setIdRoom(5);
+        assertEquals("Setter idRoom",5,r.getIdRoom());
+        r.setIdCinema(10);
+        assertEquals("Setter idCinema",10,r.setIdCinema());
+    }
 
 }
