640588a33a5c284f82257e1162643579e589a1c3
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-01 15:23:45 +0200

    testRoomModel OK

--- src/main/java/JlibPdewWdum/api/model/RoomModel.java
+++ src/main/java/JlibPdewWdum/api/model/RoomModel.java
@@ -4,4 +4,38 @@ package JlibPdewWdum.api.model;
  * Created by jlibert on 01/04/2015.
  */
 public class RoomModel {
+
+    private int idRoom;
+    private int idCinema;
+    private int nbPlaceRoom;
+
+    public RoomModel(int idRoom, int idCinema, int nbPlaceRoom) {
+        this.idRoom = idRoom;
+        this.idCinema = idCinema;
+        this.nbPlaceRoom = nbPlaceRoom;
+    }
+
+    public int getNbPlaceRoom() {
+        return nbPlaceRoom;
+    }
+
+    public int getIdRoom() {
+        return idRoom;
+    }
+
+    public int getIdCinema() {
+        return idCinema;
+    }
+
+    public void setIdRoom(int idRoom) {
+        this.idRoom = idRoom;
+    }
+
+    public void setIdCinema(int idCinema) {
+        this.idCinema = idCinema;
+    }
+
+    public void setNbPlaceRoom(int nbPlaceRoom) {
+        this.nbPlaceRoom = nbPlaceRoom;
+    }
 }
--- src/test/java/JlibPdewWdum/api/model/RoomModelTest.java
+++ src/test/java/JlibPdewWdum/api/model/RoomModelTest.java
@@ -2,27 +2,33 @@ package JlibPdewWdum.api.model;
 
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Test;
 
 public class RoomModelTest extends TestCase {
 
 
     public RoomModel r;
     @Before
-    public void setup() {
+    public void setUp() {
         r = new RoomModel(1,2,10);
     }
 
+    @Test
     public void testGetter() {
         assertEquals("getIdRoom",1,r.getIdRoom());
         assertEquals("getIdCinema",2,r.getIdCinema());
-        assertEquals("getNbPlaceRoom",10,r.getIdRomm());
+        assertEquals("getNbPlaceRoom",10,r.getNbPlaceRoom());
     }
 
+    @Test
     public void testSetter(){
         r.setIdRoom(5);
-        assertEquals("Setter idRoom",5,r.getIdRoom());
+        assertEquals("Setter idRoom", 5, r.getIdRoom());
         r.setIdCinema(10);
-        assertEquals("Setter idCinema",10,r.setIdCinema());
+        assertEquals("Setter idCinema", 10, r.getIdCinema());
+        r.setNbPlaceRoom(100);
+        assertEquals("Setter nbPlaceRoom", 100, r.getNbPlaceRoom());
+
     }
 
 }
