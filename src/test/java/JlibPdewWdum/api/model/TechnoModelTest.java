446c25ad3e6a36f7e4baf0611432e708294ffe45
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-02 16:13:21 +0200

    #19 test TechnoModel

--- src/test/java/JlibPdewWdum/api/model/TechnoModelTest.java
+++ src/test/java/JlibPdewWdum/api/model/TechnoModelTest.java
@@ -1,7 +1,26 @@
 package JlibPdewWdum.api.model;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TechnoModelTest extends TestCase {
 
+    private TechnoModel techno;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        techno = new TechnoModel(1,"IMAX");
+    }
+
+    @Test
+    public void testGetSet(){
+        assertEquals("Test getId",1,TechnoModel.getId());
+        assertEquals("Test getIntitule","IMAX",TechnoModel.getIntitule());
+        techno.setId(2);
+        techno.setIntitule("3D");
+        assertEquals("Test setId",2,TechnoModel.getId());
+        assertEquals("Test setIntitule","3D",TechnoModel.getIntitule());
+    }
 }
