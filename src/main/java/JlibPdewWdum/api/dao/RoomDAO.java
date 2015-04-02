e051c9b276b1f532a18692cc229d3d99a122feb5
Jules Libert <jules.lib@gmail.com>
2015-04-02 17:47:22 +0200

    #22 RoomDAO Tests, need models

--- src/main/java/JlibPdewWdum/api/dao/RoomDAO.java
+++ src/main/java/JlibPdewWdum/api/dao/RoomDAO.java
@@ -2,6 +2,10 @@ package JlibPdewWdum.api.dao;
 
 import JlibPdewWdum.api.model.RoomModel;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 /**
  * Created by jlibert on 01/04/2015.
  */
@@ -10,13 +14,17 @@ public class RoomDAO extends DAO<RoomModel> {
 
     @Override
     public boolean create(RoomModel obj) {
-        DatabaseManager.updateRequest("INSERT INTO Room VALUES (" + obj.getIdRoom() + "," + obj.getIdCinema() + "," + obj.getNbPlaceRoom() + ");");
+        DatabaseManager.updateRequest("INSERT INTO Room VALUES ("
+                                       + obj.getIdRoom() + ","
+                                       + obj.getIdCinema() + ","
+                                       + obj.getNbPlaceRoom() + ");");
         return false;
     }
 
     @Override
     public boolean delete(RoomModel obj) {
-        DatabaseManager.updateRequest("DELETE FROM Room WHERE idRoom = " + obj.getIdRoom() + ";");
+        DatabaseManager.updateRequest("DELETE FROM Room WHERE idRoom = "
+                                       + obj.getIdRoom() + ";");
         return false;
     }
 
@@ -27,7 +35,19 @@ public class RoomDAO extends DAO<RoomModel> {
 
     @Override
     public RoomModel find(int id) {
-        DatabaseManager.readRequest("SELECT * FROM Room WHERE idRoom = " + id + ";");
+        ResultSet rs = DatabaseManager.readRequest("SELECT * FROM Room WHERE idRoom = " + id + ";");
+        Model tmp;
+        ResultSetMetaData rm = null;
+        try {
+            rm = rs.getMetaData();
+            rs.next();
+            tmp = new RoomModel(rs.getInt(1), rs.getInt(2),
+                                 rs.getInt(3));
+            rs.close();
+            return tmp;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- src/test/java/JlibPdewWdum/api/dao/RoomDAOTest.java
+++ src/test/java/JlibPdewWdum/api/dao/RoomDAOTest.java
@@ -2,21 +2,49 @@ package JlibPdewWdum.api.dao;
 
 import JlibPdewWdum.api.model.RoomModel;
 import junit.framework.TestCase;
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class RoomDAOTest extends TestCase {
     public RoomModel r1;
     public RoomModel r2;
     public RoomModel r3;
     public RoomDAO myDAO;
-    @Before
     public void setUp() throws Exception {
         super.setUp();
-        r1 = new RoomModel(1,1,10);
-        r2 = new RoomModel(2,2,100);
-        r3 = new RoomModel(3,3,150);
+        DatabaseManager.env = DatabaseManager.Environment.TEST;
+        r1 = new RoomModel(65,1,10);
+        r2 = new RoomModel(89,2,100);
+        r3 = new RoomModel(93,3,150);
         myDAO = new RoomDAO();
+
+
+        Connection c = null;
+        String aSQLScriptFilePath1 = "database/createBDD.sql";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:database/test.db");
+
+            ScriptRunner sr = new ScriptRunner(c);
+
+            Reader readerCreate = new BufferedReader(
+                    new FileReader(aSQLScriptFilePath1));
+
+
+            // Execute script
+            sr.runScript(readerCreate);
+
+        } catch ( Exception e ) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 
     @Test
@@ -33,12 +61,7 @@ public class RoomDAOTest extends TestCase {
     }
 
     @Test
-    public void testUpdate() {
-        //TODO
-    }
-
-    @Test void testFind() {
-        //r3
-        //assertEquals("Test find",r3,myDao.find(r3.getIdRoom()));
+    public void testFind() {
+        assertEquals("Test find",r3, myDAO.find(r3.getIdRoom()));
     }
 }
