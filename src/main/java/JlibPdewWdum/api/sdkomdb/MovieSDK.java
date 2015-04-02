9c5a3e7046ed791919849f0fbd598ddecbf7ea9a
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-02 19:14:11 +0200

    Try movie sdk

--- src/main/java/JlibPdewWdum/api/sdkomdb/MovieSDK.java
+++ src/main/java/JlibPdewWdum/api/sdkomdb/MovieSDK.java
@@ -4,10 +4,13 @@ import JlibPdewWdum.api.model.MovieModel;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.apache.xmlbeans.XmlException;
+import org.apache.xmlbeans.XmlObject;
 
 import javax.ws.rs.core.MultivaluedMap;
+import javax.xml.crypto.dsig.XMLObject;
+import javax.xml.namespace.QName;
 import java.util.ArrayList;
-
 /**
  * Created by daolf on 28/03/15.
  */
@@ -20,11 +23,16 @@ public class MovieSDK {
 
 
 
-    public static MovieModel getMovieFromTitle(String title){
+    public static MovieModel getMovieFromTitle(String title) throws XmlException {
         MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
         queryParams.add("t", title);
         queryParams.add("s", "xml");
         String s = res.queryParams(queryParams).get(String.class);
+        XmlObject xml = XmlObject.Factory.parse(s);
+        return new MovieModel(xml.selectAttribute(new QName("imdbID")).toString(),
+                xml.selectAttribute(new QName("title")).toString(),
+                xml.selectAttribute(new QName("year")).toString());
+
         return null;
     }
 
