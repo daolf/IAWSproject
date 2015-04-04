76b40a173a0006c45d56b4dae010bdf294c79d64
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-04 15:51:39 +0200

    #4 minimalist implementation of /movie:id, handle of bad id missing

--- src/main/java/JlibPdewWdum/api/core/MovieController.java
+++ src/main/java/JlibPdewWdum/api/core/MovieController.java
@@ -1,24 +1,57 @@
 package JlibPdewWdum.api.core;
 
+import JlibPdewWdum.api.model.MovieModel;
 import JlibPdewWdum.api.sdkomdb.MovieSDK;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 /**
  * Created by daolf on 28/03/15.
  */
-@Path("film")
+@Path("/")
 public class MovieController {
 
+    @Path("movie")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getMovie(
+            @QueryParam("id") String id) {
+        System.out.println("---------");
+        MovieModel myMovie = MovieSDK.getMovieFromID(id);
+        ObjectMapper mapper = new ObjectMapper();
+        String s = null;
+        try
+        {
+            s = mapper.writeValueAsString(myMovie);
+        } catch (JsonGenerationException e)
+        {
+            e.printStackTrace();
+        } catch (JsonMappingException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println("---------");
+        return s;
+    }
 
+    @Path("movies")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getId(
-            @QueryParam("t") String t,
-            @QueryParam("y") int y) {
+    public String getMovies(
+            @QueryParam("id") @DefaultValue("") String id,
+            @QueryParam("t")  @DefaultValue("") String title,
+            @QueryParam("y")  @DefaultValue("0") int year) {
 
 
-        return null;
+
+        return "YOLO2";
     }
 }
