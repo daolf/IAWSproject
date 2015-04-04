aa7cdbe346b9fe42d1c07d88c25a4eee6308a007
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-04 17:24:29 +0200

    #4 test OK

--- src/test/java/JlibPdewWdum/api/core/MovieControllerTest.java
+++ src/test/java/JlibPdewWdum/api/core/MovieControllerTest.java
@@ -1,10 +1,29 @@
 package JlibPdewWdum.api.core;
 
-import junit.framework.TestCase;
 
-public class MovieControllerTest extends TestCase {
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
 
-    public void testGetId() throws Exception {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
 
+import static junit.framework.Assert.*;
+
+public class MovieControllerTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(MovieController.class);
+    }
+
+    @Test
+    public void testGetMovie() {
+        final String error = target("/movie").queryParam("id","t").request().get(String.class);
+        assertEquals("Test erreur","{ error: bad id}", error);
+
+        final String movie = target("/movie").queryParam("id","tt0178145").request().get(String.class);
+        assertEquals("Test id=tt0178145","{\"title\":\"Joan of Arc\",\"idOmdb\":\"tt0178145\",\"year\":1999}",movie);
     }
 }
