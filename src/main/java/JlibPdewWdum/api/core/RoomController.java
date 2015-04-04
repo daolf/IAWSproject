f0dba2c6153a18bd3170924887087dba3567c7b4
Pierre De Wulf <pierredewulf31@gmail.com>
2015-04-04 19:22:50 +0200

    UPD localisationDAO findByName

--- src/main/java/JlibPdewWdum/api/core/RoomController.java
+++ src/main/java/JlibPdewWdum/api/core/RoomController.java
@@ -1,7 +1,24 @@
 package JlibPdewWdum.api.core;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by jlibert on 01/04/2015.
  */
+@Path("/")
 public class RoomController {
+
+    @Path("rooms")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getRooms(
+            @QueryParam("loc") @DefaultValue("") String loc,
+            @QueryParam("tech") @DefaultValue("") String tech,
+            @QueryParam("nb") @DefaultValue("-1") int nb
+    ){
+
+
+        return null;
+    }
 }
