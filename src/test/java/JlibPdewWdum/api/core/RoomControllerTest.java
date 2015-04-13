b93d8570624f599cf56b1b8c3226d3d551665700
Jules Libert <jules.lib@gmail.com>
2015-04-13 20:00:12 +0200

    #14 ADD Start of RoomControllerTests

--- src/test/java/JlibPdewWdum/api/core/RoomControllerTest.java
+++ src/test/java/JlibPdewWdum/api/core/RoomControllerTest.java
@@ -1,7 +1,42 @@
 package JlibPdewWdum.api.core;
 
-import junit.framework.TestCase;
 
-public class RoomControllerTest extends TestCase {
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
+import javax.ws.rs.core.Application;
+
+
+public class RoomControllerTest extends JerseyTest {
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(RoomController.class);
+    }
+
+    @Test
+    public void testGetRooms() {
+        String roomsVOTest = target("/rooms").queryParam("loc", "VO").request().get(String.class);
+        String roomsVOOriginal = "[{\"room\":{\"idRoom\":1,\"idCinema\":2,\"nbPlaceRoom\":100},\"movie\":\"tt0103854\",\"nbPlaceUsed\":7,\"date\":1427478108000,\"localisation\":{\"id\":1,\"intitule\":\"VO\"},\"techno\":{\"id\":2,\"intitule\":\"IMAX\"}}]";
+
+        String roomsErrorTest = target("/rooms").queryParam("loc", "VOUZBEKISTAN").request().get(String.class);
+        String roomsErrorOriginal = "{ \"error\": \"no association found\"}";
+
+        String roomsIMAXTest = target("/rooms").queryParam("tech", "IMAX").request().get(String.class);
+        String roomsIMAXOriginal = "[{\"room\":{\"idRoom\":4,\"idCinema\":2,\"nbPlaceRoom\":402},\"movie\":\"tt2322441\",\"nbPlaceUsed\":1,\"date\":1348172976000,\"localisation\":{\"id\":2,\"intitule\":\"VF\"},\"techno\":{\"id\":2,\"intitule\":\"IMAX\"}},{\"room\":{\"idRoom\":1,\"idCinema\":2,\"nbPlaceRoom\":100},\"movie\":\"tt0103854\",\"nbPlaceUsed\":7,\"date\":1427478108000,\"localisation\":{\"id\":1,\"intitule\":\"VO\"},\"techno\":{\"id\":2,\"intitule\":\"IMAX\"}}]";
+
+        String roomsWrongParamTest = target("/rooms").queryParam("localocaloca", "VO").request().get(String.class);
+        String roomsWrongParamOriginal = "{ \"error\": \"no association found\"}";
+
+        try {
+            JSONAssert.assertEquals(roomsVOOriginal, roomsVOTest, false);
+            JSONAssert.assertEquals(roomsErrorTest, roomsErrorOriginal, false);
+            JSONAssert.assertEquals(roomsIMAXTest, roomsIMAXOriginal, false);
+            JSONAssert.assertEquals(roomsWrongParamTest, roomsWrongParamOriginal, false);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
