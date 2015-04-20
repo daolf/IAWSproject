b1f5b3cc7c3d2af2d91fc77bc22d689928353ab5
wladduma@gmail.com <wladdm@hotmail.com>
2015-04-20 12:55:33 +0200

    start test on soap #55

---
+++ src/test/java/JlibPdewWdum/soap/ApplicationTest.java
@@ -0,0 +1,13 @@
+package JlibPdewWdum.soap;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by droit on 20/04/15.
+ */
+public class ApplicationTest extends TestCase {
+    public void setUp() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/JlibPdewWdum/soap/MovieEndpointTest.java
@@ -0,0 +1,45 @@
+package JlibPdewWdum.soap;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by droit on 20/04/15.
+ */
+public class MovieEndpointTest extends TestCase {
+    private RoomsResponse roomsResponse;
+    private MovieEndpoint movieEndpoint;
+    private RoomsRequest roomsRequestOK;
+    private RoomsRequest roomsRequestKO;
+
+    public void setUp() throws Exception {
+        //create request
+        roomsResponse = new RoomsResponse();
+        movieEndpoint = new MovieEndpoint();
+        roomsRequestOK = new RoomsRequest();
+        roomsRequestKO = new RoomsRequest();
+        // existing id
+        roomsRequestOK.setIdOmdb("tt2294629");
+        // none existing id
+        roomsRequestKO.setIdOmdb("tt1111111");
+    }
+
+    @Test
+    public void testgetRooms() {
+        // good response
+        roomsResponse = movieEndpoint.getRooms(roomsRequestOK);
+        assertNotNull("Test request not null (answer)", roomsResponse);
+        assertEquals("Test good id", roomsResponse.getIdOmdb(), roomsRequestOK.getIdOmdb());
+        assertEquals("Test room of roomsResponse size", roomsResponse.room.size(), 1);
+        for (RoomsResponse.Room room : roomsResponse.room) {
+            assertEquals("Test room localisation ", room.getLocalisation(), "VF");
+            assertEquals("Test room technologie ", room.getTechno(), "LEOTECHNOLOGIES");
+        }
+
+        // bad response
+        roomsResponse = movieEndpoint.getRooms(roomsRequestKO);
+        assertNull("Test request is null ", roomsResponse);
+
+
+    }
+}
